3b562bd6a3e8d565161a3442625f2f9535322267
João Victor <dev.joaovictor@hotmail.com>
2023-10-05 14:13:00 -0300

    refact: remove react helment and change project infos

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,15 +7,10 @@ import {
   Contact,
   Footer,
 } from "../components";
-import { Helmet } from "react-helmet";
 function App() {
   return (
 
     <div className="h-auto w-full bg-background text-text-primary font-bodyFont">
-      <Helmet>
-        <title>Portfolio | João Victor</title>
-        <meta name="description" content="Portfolio | João Victor" />
-      </Helmet>
       <Navbars />
       <div className="mx-auto w-full flex flex-col px-5 py-7">
         <Presentation />
